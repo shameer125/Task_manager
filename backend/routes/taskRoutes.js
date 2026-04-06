4c004128479d38c97e615a06115e27bf678f3609
shameer125 <alishameer251@gmail.com>
2026-04-05 23:36:31 -0700

    updates in taskRouters js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,11 +3,13 @@ import Task from "../models/Task.js";
 
 const router = express.Router();
 
+
 // GET
 router.get("/", async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
 });
+
 
 // POST
 router.post("/", async (req, res) => {
@@ -15,6 +17,7 @@ router.post("/", async (req, res) => {
   await task.save();
   res.json(task);
 });
+
 
 // DELETE
 router.delete("/:id", async (req, res) => {
